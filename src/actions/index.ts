b2c641437550fc3880bb5ff770f531670069f00d
Adam Wu <drobot419@gmail.com>
2025-02-22 19:58:00 -0800

    updated, still not functioning

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,7 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { getAuth } from "firebase-admin/auth";
+import { app } from "../lib/server";
 
 export const server = {
     // remove this when we add real actions!
@@ -8,5 +10,29 @@ export const server = {
             let now = new Date()
             return now.toISOString()
         }
-    })
+    }),
+    register: defineAction({
+        input: z.object({
+            email: z.string(),
+            password: z.string(),
+            name: z.string()
+        }),
+        handler: async ({ email, password, name }) => {
+            email = email.toString();
+            password = password.toString();
+            name = name.toString();
+            const auth = getAuth(app);
+            try {
+                await auth.createUser({
+                    email,
+                    password,
+                    displayName: name,
+                });
+            } catch (error: any) {
+                return new Response(
+                    "Something went wrong",
+                    { status: 400 }
+                );
+            }
+        }})
 }
