8301dcf085193fda7a248f45d57aaf1d219bc80e
Aly <me@s5.pm>
2025-02-26 21:44:05 -0800

    Remove JPGs for SVG

---
+++ src/lib/colors.ts
@@ -0,0 +1,12 @@
+export const colors = [
+    { color: "#2baced", svgStroke: "#6d9eeb" },  // Default blue
+    { color: "#000000", svgStroke: "#000000" }, // Black
+    { color: "#00FFFF", svgStroke: "#00FFFF" },  // Cyan
+    { color: "#FF1493", svgStroke: "#FF1493" },  // Pink
+    { color: "#FF1373", svgStroke: "#FF1373" }, // Dark Pink "#FF1373"
+    { color: "#008000", svgStroke: "#008000" }, // Green
+    { color: "#FF0000", svgStroke: "#FF0000" },   // Red
+    { color: "#800080", svgStroke: "#800080" }, // Purple
+    { color: "#EEEE00", svgStroke: "#EEEE00" }, // Yellow
+    { color: "#FFA500", svgStroke: "#FFA500" }, // Orange
+];
