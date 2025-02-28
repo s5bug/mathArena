696e7b9be82e131984719be149ceba72d39d78bc
Adam Wu <drobot419@gmail.com>
2025-02-28 10:33:25 -0800

    updated the local cache storing points

--- src/lib/score.ts
+++ src/lib/score.ts
@@ -1,14 +1,13 @@
 export function getScore(): number {
     return parseInt(localStorage.getItem("userScore") || "0", 10);
-  }
+}
   
-  export function updateScore(change: number): void {
-    const newScore = getScore() + change;
-    localStorage.setItem("userScore", newScore.toString());
-  
-    
-    const scoreBox = document.getElementById("score-box");
-    if (scoreBox) {
-      scoreBox.textContent = `Score: ${newScore}`;
-    }
+export function updateScore(change: number): void {
+  const newScore = getScore() + change;
+  localStorage.setItem("userScore", newScore.toString());
+
+  const scoreBox = document.getElementById("score-box");
+  if (scoreBox) {
+    scoreBox.textContent = `Score: ${newScore}`;
   }
+}
